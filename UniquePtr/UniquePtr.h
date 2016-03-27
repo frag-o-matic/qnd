3c473e209974d87129c443267a6cf2e236f37736
frag-o-matic <frag_o_matic@gmx.com>
2016-03-27 19:50:37 +0530

    Create UniquePtr.h
    A simple smart pointer class with single-owner/no-share semantics, similar to std::unique_ptr<T>

---
+++ UniquePtr/UniquePtr.h
@@ -0,0 +1,86 @@
+#pragma once
+namespace QND
+{
+
+// class template for a simple unique pointer
+// requires C++11 features
+template<typename T>
+class UniquePtr final
+{
+public:
+    UniquePtr(T* rawPtr = nullptr)
+        :_ptr(rawPtr) {  }
+
+    ~UniquePtr() noexcept
+    {
+        if(_ptr)
+        {
+            delete _ptr;
+            _ptr = nullptr;
+        }
+    }
+
+    //copy ctor
+    UniquePtr(const UniquePtr<T>& other)
+    {
+        if(other._ptr)
+        {
+            _ptr = other._ptr;
+            other._ptr = nullptr;
+        }
+    }
+
+    //assignment operator
+    UniquePtr<T>& operator = (const UniquePtr<T>& other)
+    {
+        if(*this != other)
+        {
+            if(_ptr)
+            {
+                delete _ptr;
+                _ptr = nullptr;
+            }
+            if(other._ptr)
+            {
+                _ptr = other._ptr;
+                other._ptr = nullptr;
+            }
+        }
+        return *this;
+    }
+
+    // transparently forward address of underlying pointer
+    T* operator &() const {return _ptr;}
+
+    // deref operator
+    T& operator *()  const {return *_ptr;}
+
+    // arrow (member func/var access) operator
+    T* operator ->() const {return _ptr;}
+
+    // conversion operator to boolean
+    operator bool() const {return _ptr != nullptr;} 
+
+    // equality operator
+    bool operator == const (const UniquePtr<T>& other) {return _ptr == other._ptr;}
+    bool operator != const (const UniquePtr<T>& other) {return _ptr != other._ptr;}
+
+    // get underlying pointer without giving up ownership
+    T* get(void) const { return _ptr;}
+
+    // return underlying pointer giving up ownership
+    T* release(void) { T* temp=_ptr; _ptr=nullptr; return temp;}
+
+    // reassign
+    void reset(T* otherPtr=nullptr)
+    {
+        if(_ptr) delete _ptr; 
+        _ptr = otherPtr;
+    }
+
+private:
+    // raw pointer to be managed
+    mutable T* _ptr; 
+};
+
+}
